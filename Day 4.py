76f799c26f707d7060bad078c2d01b7d76e61e49
akshaykamath45 <73344382+akshaykamath45@users.noreply.github.com>
2021-06-12 23:58:16 +0530

    Add files via upload

---
+++ Day 4.py
@@ -0,0 +1,18 @@
+#DAY 4
+
+
+#ASSIGNMENT !
+st=input("Enter a word: \n")
+print("No pf 'y' in the word is :",st.count('y'))#Using String Function:"COUNT"
+
+
+
+#ASSIGNMENT 2
+str=input("Enter a string  :\n")
+print("Letters with Even Indexes are :",str[0::2])# STRING SLICING
+
+
+
+#ASSIGNMENT 3
+st=input("Enter a word again :\n")
+print("After swapping,The word is :",st.swapcase())#Using String Function:"SWAPCASE"
