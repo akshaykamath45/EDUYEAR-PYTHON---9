6ec6abd0cd7dd098f9ec15af7b9fa4b9d7e20cf3
akshaykamath45 <73344382+akshaykamath45@users.noreply.github.com>
2021-06-12 19:41:02 +0530

    Add files via upload

---
+++ Day 3 Simple Calculator.py
@@ -0,0 +1,19 @@
+a=int(input("Enter first number : "))
+b=int(input("Enter second number :"))
+add=a+b
+sub=a-b
+mult=a*b
+div=a/b
+rem=a%b
+fd=a//b
+power=a**b
+
+print("Addition of two number is ",add)
+print("Subtraction of two number is ",sub)
+print("Multipication of two number is ",mult)
+print("Division of two number is ",div)
+print("Remainder of two number is ",rem)
+print("Addition of two number is ",add)
+print("Floor Division of two number is ",fd)
+print("a raise to b is ",power)
+
